6e43a46bcc500558f99bb2e4549b517afb1991ee
ZiyadRW <zeadAlrouasheed@gmail.com>
2025-06-21 17:31:24 +0300

    schema and models

--- internal/modules/podcast/models/podcast.go
+++ internal/modules/podcast/models/podcast.go
@@ -1,1 +1,22 @@
 package podcast
+
+import (
+	"github.com/ziyadrw/faslah/internal/base"
+	user "github.com/ziyadrw/faslah/internal/modules/user/models"
+	"time"
+)
+
+type Podcast struct {
+	base.Model
+
+	UserID       string     `gorm:"type:uuid;not null;index" json:"user_id"`
+	Title        string     `gorm:"type:text;not null" json:"title"`
+	Description  string     `gorm:"type:text" json:"description"`
+	Tags         []string   `gorm:"type:text[]" json:"tags"`
+	MediaURL     string     `gorm:"type:text;not null" json:"media_url"`
+	SourceURL    string     `gorm:"type:text" json:"source_url"`
+	DurationSecs int        `json:"duration_secs"`
+	PublishedAt  *time.Time `json:"published_at,omitempty"`
+
+	User *user.User `gorm:"foreignKey:UserID" json:"-"`
+}
--- internal/modules/user/models/user.go
+++ internal/modules/user/models/user.go
@@ -1,1 +1,15 @@
 package user
+
+import (
+	"github.com/ziyadrw/faslah/internal/base"
+	userEnums "github.com/ziyadrw/faslah/internal/modules/user/enums"
+)
+
+type User struct {
+	base.Model
+
+	Email        string         `gorm:"uniqueIndex;not null" json:"email"`
+	PasswordHash string         `gorm:"not null" json:"-"`
+	Name         string         `gorm:"type:text" json:"name"`
+	Role         userEnums.Type `gorm:"type:text ;default:'viewer'" json:"role"`
+}
