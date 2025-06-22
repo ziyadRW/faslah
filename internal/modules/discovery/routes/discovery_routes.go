f7a58565c454b658545150158821cd63b52fa2dc
ZiyadRW <zeadAlrouasheed@gmail.com>
2025-06-22 11:35:49 +0300

    discovery routes

--- internal/modules/discovery/routes/discovery_routes.go
+++ internal/modules/discovery/routes/discovery_routes.go
@@ -1,1 +1,19 @@
 package discovery
+
+import (
+	"github.com/labstack/echo/v4"
+	discoveryHandlers "github.com/ziyadrw/faslah/internal/modules/discovery/handlers"
+	discoveryServices "github.com/ziyadrw/faslah/internal/modules/discovery/services"
+	podcastRepositories "github.com/ziyadrw/faslah/internal/modules/podcast/repositories"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(e *echo.Echo, db *gorm.DB) {
+	podcastRepo := podcastRepositories.NewPodcastRepository(db)
+	discoveryService := discoveryServices.NewDiscoveryService(podcastRepo)
+	discoveryHandler := discoveryHandlers.NewDiscoveryHandler(discoveryService)
+
+	discoveryGroup := e.Group("/discovery")
+	discoveryGroup.GET("", discoveryHandler.ListPodcasts)
+	discoveryGroup.GET("/search", discoveryHandler.SearchPodcasts)
+}
