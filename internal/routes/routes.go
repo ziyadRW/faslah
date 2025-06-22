e7a9fb3c684846710ca06b52397a181a2fa0c588
ZiyadRW <zeadAlrouasheed@gmail.com>
2025-06-22 11:17:22 +0300

    register routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	podcast "github.com/ziyadrw/faslah/internal/modules/podcast/routes"
 	user "github.com/ziyadrw/faslah/internal/modules/user/routes"
 	"gorm.io/gorm"
 )
 
 func RegisterAllRoutes(e *echo.Echo, db *gorm.DB) {
 	user.RegisterRoutes(e, db)
+	podcast.RegisterRoutes(e, db)
 	RegisterSwaggerRoutes(e)
 }
