8ee6fc8e41675cf86f524d3dd9e117e79c6986a6
ZiyadRW <zeadAlrouasheed@gmail.com>
2025-06-22 11:16:02 +0300

    watch history model chanegs

--- internal/modules/user/models/watch_history.go
+++ internal/modules/user/models/watch_history.go
@@ -1,11 +1,15 @@
 package user
 
-import "github.com/ziyadrw/faslah/internal/base"
+import (
+	"github.com/ziyadrw/faslah/internal/base"
+	"time"
+)
 
 type WatchHistory struct {
 	base.Model
 
-	UserID         string `gorm:"type:uuid;not null;index" json:"user_id"`
-	PodcastID      string `gorm:"type:uuid;not null;index" json:"podcast_id"`
-	PlaybackSecond int    `gorm:"default:0" json:"playback_second"`
+	UserID         string     `gorm:"type:uuid;not null;index" json:"user_id"`
+	PodcastID      string     `gorm:"type:uuid;not null;index" json:"podcast_id"`
+	PlaybackSecond int        `gorm:"default:0" json:"playback_second"`
+	LastPlayedAt   *time.Time `json:"last_played_at"`
 }
