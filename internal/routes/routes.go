8a3fd3c661bf37469dca4de1e719d80b7e99fe4c
ZiyadRW <zeadAlrouasheed@gmail.com>
2025-06-22 13:02:56 +0300

    my-content endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,13 +2,15 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
-	podcast "github.com/ziyadrw/faslah/internal/modules/podcast/routes"
+	cms "github.com/ziyadrw/faslah/internal/modules/cms/routes"
+	discovery "github.com/ziyadrw/faslah/internal/modules/discovery/routes"
 	user "github.com/ziyadrw/faslah/internal/modules/user/routes"
 	"gorm.io/gorm"
 )
 
 func RegisterAllRoutes(e *echo.Echo, db *gorm.DB) {
 	user.RegisterRoutes(e, db)
-	podcast.RegisterRoutes(e, db)
+	cms.RegisterRoutes(e, db)
+	discovery.RegisterRoutes(e, db)
 	RegisterSwaggerRoutes(e)
 }
